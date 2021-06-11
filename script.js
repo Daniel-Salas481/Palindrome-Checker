3c4ab667751d3b87b71c35830f8b7e44f756cad3
Daniel <dsalas6@asu.edu>
2021-06-10 18:35:31 -0700

    Palindrome function is up and working

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 'use strict';
 
-console.log('Hello World!');
+const palindrome = function (words) {
+  //Return true if given string is a palindrome
+  //Return false if string is not a palindrome
+
+  //Using replace and regex to our advantage.
+  //Also making all letters lowercase here
+  const word = words.replace(/[^0-9a-z]/gi, '').toLowerCase();
+
+  //Simply split to array, reverse it and then join it back together
+  const reversedWord = word.split('').reverse().join('');
+
+  //simply check if the two are are equal
+  //if they are, return true
+  //if not, return false
+  return word === reversedWord ? true : false;
+  //When the string is passed to function, We need to remove non-alphanumeric characters (spaces, symbols, punctuations)
+  //and turn everything to lowercase
+};
+
+const testWord = '00000600000';
+
+console.log(palindrome(testWord));
